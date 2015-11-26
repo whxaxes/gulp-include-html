b096b9368b3e21f1d0f9574e5c4adbbfbb9f81a3
wanghx <whx89768@alibaba-inc.com>
2015-11-26 18:04:30 +0800

    update

--- index.js
+++ index.js
@@ -3,52 +3,55 @@ var path = require('path');
 var parser = require("./parser");
 
 //options，传入模板变量
-module.exports = function(options){
-    options = (typeof options === "object") ? options : {};
+module.exports = function(options) {
+	options = (typeof options === "object") ? options : {};
 
-    parser.config(options);
+	parser.config(options);
 
-    var ignore = options.ignore;
-    var type = Object.prototype.toString.call(ignore);
-    var RE;
-    switch (type){
-        case  "[object Array]":
-            ignore.forEach(function(p , i){
-                ignore[i] = path.resolve(p)
-            });
-            RE = new RegExp("^(?:" + formateRe(ignore.join("|")) + ")");
-            break;
-        case "[object String]":
-            ignore = path.resolve(ignore);
-            RE = new RegExp("^" + formateRe(ignore));
-            break;
-        case "[object RegExp]":
-            RE = ignore;
-            break;
-        default : break;
-    }
+	var ignore = options.ignore;
+	var type = Object.prototype.toString.call(ignore);
+	var RE;
+	switch (type) {
+		case "[object Array]":
+			ignore.forEach(function(p, i) {
+				ignore[i] = path.resolve(p)
+			});
+			RE = new RegExp("^(?:" + formateRe(ignore.join("|")) + ")");
+			break;
+		case "[object String]":
+			ignore = path.resolve(ignore);
+			RE = new RegExp("^" + formateRe(ignore));
+			break;
+		case "[object RegExp]":
+			RE = ignore;
+			break;
+		default:
+			break;
+	}
 
-    return through.obj(function(file , enc , done){
-        var filepath = path.normalize(file.path);
-        var filename = path.basename(filepath);
+	return through.obj(function(file, enc, done) {
+		var filepath = path.normalize(file.path);
+		var filename = path.basename(filepath);
 
-        if ((RE && RE.test(filepath) && !(RE.lastIndex = 0)) || filename.match(/^_/)){
-            done();
-            return;
-        }
+		if ((RE && RE.test(filepath) && !(RE.lastIndex = 0)) || filename.match(/^_/)) {
+			done();
+			return;
+		}
 
-        var result = parser.parse(filepath , options , file.contents.toString());
+		var result = parser.parse(filepath, options, file.contents.toString());
 
-        if(result){
-            console.log('\x1B[32mbuild '+filepath+'\x1B[0m');
-            file.contents = new Buffer(result);
-            this.push(file);
-        }
+		if (result) {
+			// console.log('\x1B[32mbuild '+filepath+'\x1B[0m');
+			file.contents = new Buffer(result);
+			this.push(file);
+		}
 
-        done();
-    });
+		done();
+	});
 };
 
-function formateRe(str){
-    return str.replace(/\\|\.|\+/g , function(m){return '\\'+m});
+function formateRe(str) {
+	return str.replace(/\\|\.|\+/g, function(m) {
+		return '\\' + m
+	});
 }
--- parser.js
+++ parser.js
@@ -22,131 +22,135 @@ var inited = false;
 var configName;
 
 module.exports = {
-    config : function(options){
-        options = options || {};
+  config: function (options) {
+    options = options || {};
 
-        if(options.ejs){
-            for(var k in options.ejs){
-                ejs[k] = options.ejs[k];
-            }
-        }
-
-        suffix = options.suffix || suffix;
-        include = options.include || include;
-        baseDir = options.baseDir;
-        configName = options.configName || "config.js";
-
-        INC_RE = new RegExp("([ \\t]*)" + include + INC_RE_STR , 'g');
-        inited = true;
-    },
-
-    //filepath：文件路径，options包括模板变量及部分参数，content:文件内容
-    parse : function(filepath , options , content){
-        if(!inited){this.config()}
-
-        if(arguments.length===2 && (typeof options ==="string")){
-            content = options;
-            options = {};
-        }
-
-        content = content || getFileString(filepath);
-        options = options || {};
-
-        if(!content) return "";
-
-        return combine(content , filepath , options);
+    if (options.ejs) {
+      for (var k in options.ejs) {
+        ejs[k] = options.ejs[k];
+      }
     }
+
+    suffix = options.suffix || suffix;
+    include = options.include || include;
+    baseDir = options.baseDir;
+    configName = options.configName || "config.js";
+
+    INC_RE = new RegExp("([ \\t]*)" + include + INC_RE_STR, 'g');
+    inited = true;
+  },
+
+  //filepath：文件路径，options包括模板变量及部分参数，content:文件内容
+  parse: function (filepath, options, content) {
+    if (!inited) {
+      this.config()
+    }
+
+    if (arguments.length === 2 && (typeof options === "string")) {
+      content = options;
+      options = {};
+    }
+
+    content = content || getFileString(filepath);
+    options = options || {};
+
+    if (!content) return "";
+
+    return combine(content, filepath, options);
+  }
 };
 
 //将@@include替换成相应文件
-function combine(content , filePath , opt){
-    var fileUrl ,   //include的文件地址
-        templateFile;  //include的文件内容
+function combine(content, filePath, opt) {
+  var fileUrl, //include的文件地址
+    templateFile; //include的文件内容
 
-    //如果文件目录下存在config.js，则将以ejs模板的形式包装
-    var configPath = path.dirname(filePath) + path.sep + configName;
-    if (fs.existsSync(configPath)) {
-        var delimiter = ejs.delimiter || "%";
-        var configFile = "<" + delimiter + fs.readFileSync(configPath).toString() + delimiter + ">";
-        content = configFile + content;
+  //如果文件目录下存在config.js，则将以ejs模板的形式包装
+  var configPath = path.dirname(filePath) + path.sep + configName;
+  if (fs.existsSync(configPath)) {
+    var delimiter = ejs.delimiter || "%";
+    var configFile = "<" + delimiter + fs.readFileSync(configPath).toString() + delimiter + ">";
+    content = configFile + content;
+  }
+
+  opt = opt || {};
+
+  try {
+    var result = ejs.render(content, opt);
+  } catch (e) {
+    console.log("\x1B[31mbuild " + filePath + " fail\x1B[0m");
+    console.log("\x1B[31mEjs error：" + e.message + "\x1B[0m");
+    return;
+  }
+
+  result = result.replace(INC_RE, function (msg) {
+    var obj, nobj;
+    var space = RegExp.$1;
+
+    fileUrl = RegExp.$2;
+    obj = RegExp.$3 || "{}";
+
+    if (!(typeof baseDir === "string") || NOBASE_RE.test(fileUrl)) {
+      fileUrl = url.resolve(filePath, fileUrl);
+    } else {
+      fileUrl = baseDir + "/" + fileUrl;
     }
 
-    opt = opt || {};
+    //如果文件没有文件类型扩展名，则加上
+    fileUrl += (new RegExp("." + suffix + "$")).test(fileUrl) ? "" : ("." + suffix);
 
-    try{
-        var result = ejs.render(content , opt);
-    }catch(e){
-        console.log("\x1B[31mbuild " + filePath + " fail\x1B[0m");
-        console.log("\x1B[31mEjs error：" + e.message + "\x1B[0m");
-        return;
+    if (!(templateFile = getFileString(fileUrl))) return msg;
+
+    //获取@@include里传入的参数，并转成对象
+    try {
+      obj = eval("(" + obj.replace(/\r\n/, '') + ")");
+    } catch (e) {
+      obj = {};
     }
 
-    result = result.replace(INC_RE , function(msg){
-        var obj , nobj;
-        var space = RegExp.$1;
+    //将参数对象拷贝，并且与include里传入的参数合并
+    nobj = deepCopy(opt);
 
-        fileUrl = RegExp.$2;
-        obj = RegExp.$3 || "{}";
+    for (var k in obj) {
+      nobj[k] = obj[k];
+    }
 
-        if(!(typeof baseDir==="string") || NOBASE_RE.test(fileUrl)){
-            fileUrl = url.resolve(filePath, fileUrl);
-        }else {
-            fileUrl = baseDir + "/" + fileUrl;
-        }
+    // 用于保证格式
+    var result = combine(templateFile, fileUrl, nobj);
+    var lines = result.split(/\r?\n/g);
 
-        //如果文件没有文件类型扩展名，则加上
-        fileUrl += (new RegExp("." + suffix + "$")).test(fileUrl) ? "" : ("." + suffix);
-
-        if(!(templateFile = getFileString(fileUrl))) return msg;
-
-        //获取@@include里传入的参数，并转成对象
-        try{
-            obj = eval("(" + obj.replace(/\r\n/, '') + ")");
-        }catch(e){
-            obj = {};
-        }
-
-        //将参数对象拷贝，并且与include里传入的参数合并
-        nobj = deepCopy(opt);
-
-        for(var k in obj){ nobj[k] = obj[k]; }
-
-        // 用于保证格式
-        var result = combine(templateFile , fileUrl , nobj);
-        var lines = result.split(/\r?\n/g);
-
-        result = space + lines.join("\n" + space);
-
-        return result;
-    });
+    result = space + lines.join("\n" + space);
 
     return result;
+  });
+
+  return result;
 }
 
 //获取文件字符串
-function getFileString(filepath){
-    if (/(?:\/|\\)$/.test(filepath) || !fs.existsSync(filepath)) {
-        console.log("\x1B[31mfile is not exist：" + filepath + "\x1B[0m");
-        return null;
-    } else {
-        return fs.readFileSync(filepath).toString()
-    }
+function getFileString(filepath) {
+  if (/(?:\/|\\)$/.test(filepath) || !fs.existsSync(filepath)) {
+    console.log("\x1B[31mfile is not exist：" + filepath + "\x1B[0m");
+    return null;
+  } else {
+    return fs.readFileSync(filepath).toString()
+  }
 }
 
 //深度拷贝
-function deepCopy(obj){
-    var result = {};
-    var type = toString.call(obj);
+function deepCopy(obj) {
+  var result = {};
+  var type = toString.call(obj);
 
-    if(!obj || (type === "[object RegExp]") || typeof obj !== "object")
-        return obj;
+  if (!obj || (type === "[object RegExp]") || typeof obj !== "object")
+    return obj;
 
-    if(type === "[object Array]")
-        return slice.call(obj);
+  if (type === "[object Array]")
+    return slice.call(obj);
 
-    for(var k in obj){
-        result[k] = deepCopy(obj[k])
-    }
+  for (var k in obj) {
+    result[k] = deepCopy(obj[k])
+  }
 
-    return result;
+  return result;
 }
